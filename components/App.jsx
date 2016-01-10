33a4bc7572ea3d7d7d3b99b10cd5315189039fc5
Calvin Woo <calvinwoo32@gmail.com>
2016-01-10 00:43:37 -0800

    Add page title

--- components/App.jsx
+++ components/App.jsx
@@ -3,8 +3,14 @@ import {Link} from 'react-router';
 
 const App = ({children}) => {
   return (
-    <div style={{padding: '20px 20px'}}>
+    <div className='container'>
       <header style={{marginBottom: 30}}>
+        <h3>redux-routing-demo</h3>
+        <p>
+          <a href='https://github.com/calvinwoo/redux-routing-demo'>
+            https://github.com/calvinwoo/redux-routing-demo
+          </a>
+        </p>
         <h4>Table of Contents</h4>
         <ul className='list-inline'>
           <li><Link to='/count'>Count</Link></li>
